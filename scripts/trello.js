88d172dc9b41f5f848b0cc75e0b71237fef065a7
Natasha Jones <tasha.k.jones@gmail.com>
2016-09-21 20:24:25 -0400

    It's Alivegit add .

---
+++ scripts/trello.js
@@ -0,0 +1,6 @@
+module.exports = function(robot) {
+  // add chron
+  // hubot should ping trello every x amount of time to check
+  // the number of items in the Incoming (Prod) list or Prioritized Backlog (Eng)?
+  // if the number is greater than x, post message to Flowdock 
+}
