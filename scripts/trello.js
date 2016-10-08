c3e1c1b7f2d52c87428273e5bbf32bb578b4f37d
Natasha Jones <tasha.k.jones@gmail.com>
2016-10-08 18:34:09 -0400

    Get cards from trello list

--- scripts/trello.js
+++ scripts/trello.js
@@ -5,48 +5,22 @@ module.exports = function(robot) {
   tz = 'America/New_York'
 
   return new cronJob('* * * * * *', trelloBugBeGone(robot), null, true, tz);
-
 }
 
-var trelloBugBeGone = function() {
+var trelloBugBeGone = function(robot) {
+  var list = "https://api.trello.com/1/lists/57c9b8a1e1b0111e5f373301/cards"
+  var auth = "?key="+process.env.HUBOT_TRELLO_KEY+"&token="+process.env.HUBOT_TRELLO_TOKEN
+
+  var getCards = robot.http(list + auth)
+                  .get()(function(err, res, body) {
+                      res = JSON.parse(body);
+                      if (res.length >= 1) {
+                        console.log("You guys have some bugs to fix!")
+                      }
+                    // then figure out posting to flowdock
+                  })
+
   return function() {
-    return console.log("I'm Alive!!")
+    console.log(getCards)
   }
-
-  // var bugList = function()
 }
-//
-// new CronJob('* * * * * *', function() {
-//   console.log('You will see this message every second');
-// }, null, true, 'America/Los_Angeles');
-
-
-// var cronJob, tz;
-//
-// cronJob = require('cron').CronJob;
-//
-// tz = 'America/Los_Angeles';
-//
-// new cronJob('0 0 9 * * 1-5', workdaysNineAm, null, true, tz);
-//
-// new cronJob('0 */5 * * * *', everyFiveMinutes, null, true, tz);
-//
-// module.exports = function(robot) {
-//   var cronJob;
-//   cronJob = require('cron').CronJob;
-//   return new cronJob('0 */1 * * * *', everyMinute(robot), null, true);
-// };
-//
-// var everyMinute = function(robot) {
-//   return function() {
-//     return robot.messageRoom('#billing', 'hey brah!');
-//   };
-// };
-
-
-
-  // the number of items in the Incoming (Prod) list or Prioritized Backlog (Eng)?
-  // if the number is greater than x, post message to Flowdock
-  // there's an example here https://leanpub.com/automation-and-monitoring-with-hubot/read
-
-  // runs at 9am and 12pm every Monday-Friday 00 00 09,12 * * 1-5
