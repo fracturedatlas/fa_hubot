8f359bdec2e707a9f230b68be198e0c0dae19c48
Natasha Jones <tasha.k.jones@gmail.com>
2016-11-02 07:04:50 -0400

    add images

--- scripts/trello.js
+++ scripts/trello.js
@@ -10,14 +10,13 @@ module.exports = function(robot) {
 var trelloBugBeGone = function(robot) {
   var list = "https://api.trello.com/1/lists/58063da38e3169695b4e114e/cards"
   var auth = "?key="+process.env.HUBOT_TRELLO_KEY+"&token="+process.env.HUBOT_TRELLO_TOKEN
-  var message = "@tasha!! I'm Alive <3"
 
   var getCards = robot.http(list + auth)
                   .get()(function(err, res, body) {
                       response = JSON.parse(body);
                       if (response.length >= 1) {
                         console.log("You guys have some bugs to fix!")
-                        robot.messageRoom('fracturedatlas/bottesting')
+                        robot.messageRoom('fracturedatlas/bottesting', "@tasha!! I'm Alive <3")
                       }
                   })
 
