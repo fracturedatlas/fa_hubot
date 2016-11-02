1bc054a2b68c4ba17e307acefc6f010cffb01979
Natasha Jones <tasha.k.jones@gmail.com>
2016-11-02 06:49:30 -0400

    Update the list id

--- scripts/trello.js
+++ scripts/trello.js
@@ -8,19 +8,21 @@ module.exports = function(robot) {
 }
 
 var trelloBugBeGone = function(robot) {
-  var list = "https://api.trello.com/1/lists/57c9b8a1e1b0111e5f373301/cards"
+  var list = "https://api.trello.com/1/lists/58063da38e3169695b4e114e/cards"
   var auth = "?key="+process.env.HUBOT_TRELLO_KEY+"&token="+process.env.HUBOT_TRELLO_TOKEN
+  var message = "@tasha!! I'm Alive <3"
 
   var getCards = robot.http(list + auth)
                   .get()(function(err, res, body) {
-                      res = JSON.parse(body);
-                      if (res.length >= 1) {
+                      response = JSON.parse(body);
+                      if (response.length >= 1) {
                         console.log("You guys have some bugs to fix!")
+                        robot.messageRoom('fracturedatlas/bottesting')
                       }
-                    // then figure out posting to flowdock
                   })
 
   return function() {
+    console.log("HEY!")
     console.log(getCards)
   }
 }
