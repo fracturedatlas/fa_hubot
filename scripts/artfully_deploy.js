ba9e672d84af55427f3ed5decef8c2dd182f7052
Dan Porter <wolfpakz@gmail.com>
2017-01-15 11:26:02 -0800

    First stab at converting our API experiments to code
    Evening of 1/11/2017

--- scripts/artfully_deploy.js
+++ scripts/artfully_deploy.js
@@ -1,4 +1,3 @@
-
 // command: @hubot artfully deploy 585
 // output: Sure boss, one second while I deploy PR 585 ..
 
@@ -22,32 +21,83 @@
 //   Upload tarball somewhere temporary (S3)
 //   Create a Heroku build using the checksum and temporary tarball link
 
-
 // Heroku API:
 //    Must include header - Accept: application/vnd.heroku+json; version=3
 
 // TODO:
 //   Figure out how to make Heroku read the app.json when we trigger a build
 
-function deployPullRequest(number) {
-  var app = createReviewApp(number)
+module.exports = function (robot) {
 
-  removePgAddon(app);
-  provisionDatabase(app);
-  provisionSolrIndex(app);
-  configureApp(app);
-  scaleProcesses(app);
-}
+  function deployPullRequest(number) {
+    var app = createReviewApp(number);
+    removePgAddon(app);
+    provisionDatabase(app);
+    provisionSolrIndex(app);
+    configureApp(app);
+    scaleProcesses(app);
+  }
 
-function getPullRequest(number) {
-  // GET /repos/fracturedatlas/artful.ly/pulls/:number
-  // response['head']['sha']
-}
+  function getPullRequest(number) {
+    // GET /repos/fracturedatlas/artful.ly/pulls/:number
+    // response['head']['sha']
+    robot.http("https://api.github.com/repos/fracturedatlas/artful.ly/pulls/" + number)
+      .header('Accept', 'application/vnd.github.v3+json')
+      .header('Authorize', 'Basic Z2VtaW5pc2JzOmI5ZWZlMWY0OWM4ZWFkMDUzZTIxZmNlYjg0NjYyOTRjYjE2YWZiOWQ=')
+      .get(function (err, res, body) {
+        var data = null;
+        robot.logger.info('-- Error');
+        robot.logger.info(err);
+        robot.logger.info('--');
 
-function createReviewApp(prNumber) {
-  // GET /repos/:owner/:repo/git/blobs/:sha
-  var pr = getPullRequest(number)
-  var blob = getGithubBlob(pr['head']['sha'])
-  createHerokuBuild(blob)
-}
+        robot.logger.info('--- Body');
+        robot.logger.info(body);
+        robot.logger.info('--');
 
+
+        if (err) {
+          robot.logger.info("Er, whoops: ");
+          robot.logger.info(err);
+          return;
+        }
+
+        try {
+          data = JSON.parse(body);
+          robot.logger.info("PR SHA is " + data['head']['sha']);
+        } catch (error) {
+          robot.logger.info('--- JSON error');
+          robot.logger.info(err);
+          robot.logger.info('--');
+          robot.logger.info("Had a problem parsing JSON - Sorry.. :(");
+          return;
+        }
+      });
+  }
+
+  function createReviewApp(number) {
+    // GET /repos/:owner/:repo/git/blobs/:sha
+    var pr = getPullRequest(number);
+    var blob = getGithubBlob(pr['head']['sha']);
+    createHerokuBuild(blob)
+  }
+
+  function processCommand(res) {
+    var number = res.match[2]
+    res.send('Deploying PR ' + number);
+    deployPullRequest(number);
+    return;
+  }
+
+  function errorHandler(err, res) {
+    robot.logger.error("DOES NOT COMPUTE")
+    robot.logger.error(err);
+
+    if (res) {
+      res.reply("DOES NOT COMPUTE")
+    }
+  }
+
+  /////////////////////////////
+  robot.error(errorHandler);
+  robot.respond(/artfully deploy (pr |PR )?(\d+)/, processCommand);
+};
